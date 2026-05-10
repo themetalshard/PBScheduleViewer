5a46f93e530d6a460713440ebb2f8a1674f2afff
TheMetalShard <themetalshard@softmodd.ing>
2026-05-10 15:15:02 +0200

    Initial Commit

---
+++ PB Schedule Viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PB Schedule Viewer
+//
+//  Created by nexgen on 5/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PB Schedule Viewer/PB_Schedule_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PB_Schedule_ViewerApp.swift
+//  PB Schedule Viewer
+//
+//  Created by nexgen on 5/10/26.
+//
+
+import SwiftUI
+
+@main
+struct PB_Schedule_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
